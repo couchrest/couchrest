9a39d0b1cb0c59b0447862656f7d894ef0048978
Chris Anderson <jchris@grabb.it>
2008-06-12 09:09:56 -0700

    experimental interface to fulltext search

--- lib/database.rb
+++ lib/database.rb
@@ -24,6 +24,12 @@ class CouchRest
       url = CouchRest.paramify_url "#{@root}/_view/#{name}", params
       CouchRest.get url
     end
+
+    # experimental
+    def search params = nil
+      url = CouchRest.paramify_url "#{@root}/_search", params
+      CouchRest.get url
+    end
   
     def get id
       slug = CGI.escape(id) 
