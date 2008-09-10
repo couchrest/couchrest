a2a78dcf5f6db59815bce1b826d04eaf48924c10
Chris Anderson <jchris@grabb.it>
2008-09-10 14:37:30 -0700

    better json time format

--- lib/couchrest.rb
+++ lib/couchrest.rb
@@ -8,4 +8,12 @@ require File.dirname(__FILE__) + '/pager'
 require File.dirname(__FILE__) + '/file_manager'
 require File.dirname(__FILE__) + '/streamer'
 
+# this has to come after the JSON gem
 
+# this date format sorts lexicographically
+# and is compatible with Javascript's new Date(time_string) constructor
+class Time
+  def to_json(options = nil)
+    %("#{strftime("%Y/%m/%d %H:%M:%S %z")}")
+  end
+end
