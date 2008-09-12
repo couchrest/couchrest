7028f7f7b36ac7094378269cb976d120122578e8
Chris Anderson <jchris@grabb.it>
2008-09-11 21:16:01 -0700

    ganked load path from merb

--- lib/couchrest.rb
+++ lib/couchrest.rb
@@ -2,6 +2,11 @@ require "rubygems"
 require 'json'
 require 'rest_client'
 
+$:.unshift File.dirname(__FILE__) unless
+  $:.include?(File.dirname(__FILE__)) ||
+  $:.include?(File.expand_path(File.dirname(__FILE__)))
+  
+  
 require 'couchrest/monkeypatches'
 
 module CouchRest
