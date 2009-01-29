d9fe6ba374acf13b0a78395a36801c0cbec20d45
Matt Aimonetti <mattaimonetti@gmail.com>
2009-01-28 22:55:42 -0800

    Started the refactoring work on couchrest.
    * A server can have multiple defined available databases set to be used by documents (think DM repos)
    * A server can have a default database so documents can easily share the same db connection
    * Let a document class have a default database to use
    * Give access to a document uri
    * extracted some of the document features to a mixin

--- lib/couchrest/core/document.rb
+++ lib/couchrest/core/document.rb
@@ -14,47 +14,20 @@ module CouchRest
   end
   
   class Document < Response
+    include CouchRest::Mixins::Views
 
     attr_accessor :database
-
-    # alias for self['_id']
-    def id
-      self['_id']
+    @@database = nil
+    
+    # override the CouchRest::Model-wide default_database
+    # This is not a thread safe operation, do not change the model
+    # database at runtime.
+    def self.use_database(db)
+      @@database = db
     end
-
-    # alias for self['_rev']      
-    def rev
-      self['_rev']
-    end
-
-    # returns true if the document has never been saved
-    def new_document?
-      !rev
-    end
-
-    # Saves the document to the db using create or update. Also runs the :save
-    # callbacks. Sets the <tt>_id</tt> and <tt>_rev</tt> fields based on
-    # CouchDB's response.
-    # If <tt>bulk</tt> is <tt>true</tt> (defaults to false) the document is cached for bulk save.
-    def save(bulk = false)
-      raise ArgumentError, "doc.database required for saving" unless database
-      result = database.save_doc self, bulk
-      result['ok']
-    end
-
-    # Deletes the document from the database. Runs the :delete callbacks.
-    # Removes the <tt>_id</tt> and <tt>_rev</tt> fields, preparing the
-    # document to be saved to a new <tt>_id</tt>.
-    # If <tt>bulk</tt> is <tt>true</tt> (defaults to false) the document won't 
-    # actually be deleted from the db until bulk save.
-    def destroy(bulk = false)
-      raise ArgumentError, "doc.database required to destroy" unless database
-      result = database.delete_doc(self, bulk)
-      if result['ok']
-        self['_rev'] = nil
-        self['_id'] = nil
-      end
-      result['ok']
+    
+    def self.database
+      @@database
     end
     
     # copies the document to a new id. If the destination id currently exists, a rev must be provided.
@@ -73,6 +46,23 @@ module CouchRest
       raise ArgumentError, "doc.database required to copy" unless database
       result = database.move_doc(self, dest)
       result['ok']
+    end
+    
+    # Returns the CouchDB uri for the document
+    def uri(append_rev = false)
+      return nil if new_document?
+      couch_uri = "http://#{database.uri}/#{CGI.escape(id)}"
+      if append_rev == true
+        couch_uri << "?rev=#{rev}"
+      elsif append_rev.kind_of?(Integer)
+        couch_uri << "?rev=#{append_rev}"
+      end
+      couch_uri
+    end
+    
+    # Returns the document's database
+    def database
+      @database || self.class.database
     end
     
     # saves an attachment directly to couchdb
@@ -97,6 +87,5 @@ module CouchRest
       result['ok']
     end
   end
-
   
 end
