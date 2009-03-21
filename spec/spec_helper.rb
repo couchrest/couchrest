fb7b33b1a70b2dc839b83364308963a3132dd098
Will Leinweber <will@bitfission.com>
2009-03-21 12:14:42 +0800

    Specs now clean up after themselves by deleting the test databases
    Signed-off-by: Matt Aimonetti <mattaimonetti@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,3 +24,15 @@ def reset_test_db!
   db.recreate! rescue nil
   db
 end
+
+Spec::Runner.configure do |config|
+  config.before(:all) { reset_test_db! }
+  
+  config.after(:all) do
+    cr = TEST_SERVER
+    test_dbs = cr.databases.select { |db| db =~ /^#{TESTDB}/ }
+    test_dbs.each do |db|
+      cr.database(db).delete! rescue nil
+    end
+  end
+end
