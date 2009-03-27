abd19f7b68bd3b70d902a57b9395879511956bfe
Matt Aimonetti <mattaimonetti@gmail.com>
2009-03-27 11:27:44 -0700

    bumped release to 0.23 (lots of fixes)

--- lib/couchrest.rb
+++ lib/couchrest.rb
@@ -28,7 +28,7 @@ require 'couchrest/monkeypatches'
 
 # = CouchDB, close to the metal
 module CouchRest
-  VERSION    = '0.22' unless self.const_defined?("VERSION")
+  VERSION    = '0.23' unless self.const_defined?("VERSION")
   
   autoload :Server,       'couchrest/core/server'
   autoload :Database,     'couchrest/core/database'
