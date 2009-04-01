ef3b0527c9e250e5f438971e2e987135ca70e6dd
Geoff Buesing <geoff@lifesnapz.com>
2009-04-01 16:00:36 -0500

    Hash#=== monkeypatch: don't add special behavior to subclasses, i.e. CouchRest documents

--- lib/couchrest/support/rails.rb
+++ lib/couchrest/support/rails.rb
@@ -7,7 +7,7 @@ class Hash
   # Hack so that CouchRest::Document, which descends from Hash,
   # doesn't appear to Rails routing as a Hash of options
   def self.===(other)
-    return false if other.is_a?(CouchRest::Document)
+    return false if self == Hash && other.is_a?(CouchRest::Document)
     super
   end
 end
