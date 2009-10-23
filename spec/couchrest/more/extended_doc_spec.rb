164ef6bc9dab0f3e8a741fa316bfa174b1250288
Matt Aimonetti <mattaimonetti@gmail.com>
2009-10-23 15:47:20 -0200

    fixed a encoding bug with ruby1.9

--- spec/couchrest/more/extended_doc_spec.rb
+++ spec/couchrest/more/extended_doc_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require File.expand_path("../../../spec_helper", __FILE__)
 require File.join(FIXTURE_PATH, 'more', 'article')
 require File.join(FIXTURE_PATH, 'more', 'course')
