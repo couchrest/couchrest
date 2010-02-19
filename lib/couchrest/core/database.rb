a856e98a6afdc0f0d7952afbb37ad8df7685678b
Noah Zoschke <noah@heroku.com>
2010-02-18 18:26:04 -0800

    remove double CGI escape when PUTting an attachment
    Signed-off-by: Marcos Tapajós <tapajos@gmail.com>

--- lib/couchrest/core/database.rb
+++ lib/couchrest/core/database.rb
@@ -107,7 +107,6 @@ module CouchRest
     # PUT an attachment directly to CouchDB
     def put_attachment(doc, name, file, options = {})
       docid = escape_docid(doc['_id'])
-      name = CGI.escape(name)
       uri = url_for_attachment(doc, name)
       JSON.parse(HttpAbstraction.put(uri, file, options))
     end
