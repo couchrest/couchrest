3d3eff0362ac64d053ad3f0e309ef88887ed5110
Geoff Buesing <geoff@timelines.com>
2010-07-01 15:36:27 -0500

    Alias couchrest_inheritable_* methods to extlib_inheritable_* for backwards compatibility

--- lib/couchrest/helper/inheritable_attributes.rb
+++ lib/couchrest/helper/inheritable_attributes.rb
@@ -24,83 +24,104 @@
 
 module CouchRest
   module InheritableAttributes
-    # Defines class-level inheritable attribute reader. Attributes are available to subclasses,
-    # each subclass has a copy of parent's attribute.
-    #
-    # @param *syms<Array[#to_s]> Array of attributes to define inheritable reader for.
-    # @return <Array[#to_s]> Array of attributes converted into inheritable_readers.
-    #
-    # @api public
-    #
-    # @todo Do we want to block instance_reader via :instance_reader => false
-    # @todo It would be preferable that we do something with a Hash passed in
-    #   (error out or do the same as other methods above) instead of silently
-    #   moving on). In particular, this makes the return value of this function
-    #   less useful.
-    def couchrest_inheritable_reader(*ivars)
-      instance_reader = ivars.pop[:reader] if ivars.last.is_a?(Hash)
+    
+    def self.extended(base)
+      # extend first so that we can create aliases
+      base.extend ClassMethods
+      
+      # alias couchrest_inheritable_* methods to extlib_inheritable_* for backwards compatibility:
+      unless base.respond_to?(:extlib_inheritable_reader)
+        class << base; alias_method :extlib_inheritable_reader, :couchrest_inheritable_reader; end
+      end
+      
+      unless base.respond_to?(:extlib_inheritable_writer)
+        class << base; alias_method :extlib_inheritable_writer, :couchrest_inheritable_writer; end
+      end
+      
+      unless base.respond_to?(:extlib_inheritable_accessor)
+        class << base; alias_method :extlib_inheritable_accessor, :couchrest_inheritable_accessor; end
+      end
+    end
+    
+    module ClassMethods
+      # Defines class-level inheritable attribute reader. Attributes are available to subclasses,
+      # each subclass has a copy of parent's attribute.
+      #
+      # @param *syms<Array[#to_s]> Array of attributes to define inheritable reader for.
+      # @return <Array[#to_s]> Array of attributes converted into inheritable_readers.
+      #
+      # @api public
+      #
+      # @todo Do we want to block instance_reader via :instance_reader => false
+      # @todo It would be preferable that we do something with a Hash passed in
+      #   (error out or do the same as other methods above) instead of silently
+      #   moving on). In particular, this makes the return value of this function
+      #   less useful.
+      def couchrest_inheritable_reader(*ivars)
+        instance_reader = ivars.pop[:reader] if ivars.last.is_a?(Hash)
 
-      ivars.each do |ivar|
-        self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          def self.#{ivar}
-            return @#{ivar} if self.object_id == #{self.object_id} || defined?(@#{ivar})
-            ivar = superclass.#{ivar}
-            return nil if ivar.nil? && !#{self}.instance_variable_defined?("@#{ivar}")
-            @#{ivar} = ivar && !ivar.is_a?(Module) && !ivar.is_a?(Numeric) && !ivar.is_a?(TrueClass) && !ivar.is_a?(FalseClass) ? ivar.dup : ivar
-          end
-        RUBY
-        unless instance_reader == false
+        ivars.each do |ivar|
           self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-            def #{ivar}
-              self.class.#{ivar}
+            def self.#{ivar}
+              return @#{ivar} if self.object_id == #{self.object_id} || defined?(@#{ivar})
+              ivar = superclass.#{ivar}
+              return nil if ivar.nil? && !#{self}.instance_variable_defined?("@#{ivar}")
+              @#{ivar} = ivar && !ivar.is_a?(Module) && !ivar.is_a?(Numeric) && !ivar.is_a?(TrueClass) && !ivar.is_a?(FalseClass) ? ivar.dup : ivar
             end
           RUBY
-        end
-      end
-    end unless Class.respond_to?(:couchrest_inheritable_reader)
-
-    # Defines class-level inheritable attribute writer. Attributes are available to subclasses,
-    # each subclass has a copy of parent's attribute.
-    #
-    # @param *syms<Array[*#to_s, Hash{:instance_writer => Boolean}]> Array of attributes to
-    #   define inheritable writer for.
-    # @option syms :instance_writer<Boolean> if true, instance-level inheritable attribute writer is defined.
-    # @return <Array[#to_s]> An Array of the attributes that were made into inheritable writers.
-    #
-    # @api public
-    #
-    # @todo We need a style for class_eval <<-HEREDOC. I'd like to make it
-    #   class_eval(<<-RUBY, __FILE__, __LINE__), but we should codify it somewhere.
-    def couchrest_inheritable_writer(*ivars)
-      instance_writer = ivars.pop[:writer] if ivars.last.is_a?(Hash)
-      ivars.each do |ivar|
-        self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          def self.#{ivar}=(obj)
-            @#{ivar} = obj
+          unless instance_reader == false
+            self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+              def #{ivar}
+                self.class.#{ivar}
+              end
+            RUBY
           end
-        RUBY
-        unless instance_writer == false
-          self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-            def #{ivar}=(obj) self.class.#{ivar} = obj end
-          RUBY
         end
-
-        self.send("#{ivar}=", yield) if block_given?
       end
-    end unless Class.respond_to?(:couchrest_inheritable_writer)
 
-    # Defines class-level inheritable attribute accessor. Attributes are available to subclasses,
-    # each subclass has a copy of parent's attribute.
-    #
-    # @param *syms<Array[*#to_s, Hash{:instance_writer => Boolean}]> Array of attributes to
-    #   define inheritable accessor for.
-    # @option syms :instance_writer<Boolean> if true, instance-level inheritable attribute writer is defined.
-    # @return <Array[#to_s]> An Array of attributes turned into inheritable accessors.
-    #
-    # @api public
-    def couchrest_inheritable_accessor(*syms, &block)
-      couchrest_inheritable_reader(*syms)
-      couchrest_inheritable_writer(*syms, &block)
-    end unless Class.respond_to?(:couchrest_inheritable_accessor)
+      # Defines class-level inheritable attribute writer. Attributes are available to subclasses,
+      # each subclass has a copy of parent's attribute.
+      #
+      # @param *syms<Array[*#to_s, Hash{:instance_writer => Boolean}]> Array of attributes to
+      #   define inheritable writer for.
+      # @option syms :instance_writer<Boolean> if true, instance-level inheritable attribute writer is defined.
+      # @return <Array[#to_s]> An Array of the attributes that were made into inheritable writers.
+      #
+      # @api public
+      #
+      # @todo We need a style for class_eval <<-HEREDOC. I'd like to make it
+      #   class_eval(<<-RUBY, __FILE__, __LINE__), but we should codify it somewhere.
+      def couchrest_inheritable_writer(*ivars)
+        instance_writer = ivars.pop[:writer] if ivars.last.is_a?(Hash)
+        ivars.each do |ivar|
+          self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def self.#{ivar}=(obj)
+              @#{ivar} = obj
+            end
+          RUBY
+          unless instance_writer == false
+            self.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+              def #{ivar}=(obj) self.class.#{ivar} = obj end
+            RUBY
+          end
+
+          self.send("#{ivar}=", yield) if block_given?
+        end
+      end
+
+      # Defines class-level inheritable attribute accessor. Attributes are available to subclasses,
+      # each subclass has a copy of parent's attribute.
+      #
+      # @param *syms<Array[*#to_s, Hash{:instance_writer => Boolean}]> Array of attributes to
+      #   define inheritable accessor for.
+      # @option syms :instance_writer<Boolean> if true, instance-level inheritable attribute writer is defined.
+      # @return <Array[#to_s]> An Array of attributes turned into inheritable accessors.
+      #
+      # @api public
+      def couchrest_inheritable_accessor(*syms, &block)
+        couchrest_inheritable_reader(*syms)
+        couchrest_inheritable_writer(*syms, &block)
+      end
+    end
   end
 end
